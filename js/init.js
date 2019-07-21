518379bc595644da7afc32116153dccf9c97ccc0
Andrew Chau <andrewchau333@gmail.com>
2019-07-21 09:00:08 -0400

    Added smooth scrollling

--- js/init.js
+++ js/init.js
@@ -2,6 +2,12 @@
   $(function(){
 
     $('.parallax').parallax();
-
+    $('a').filter(function() { // smooth scroll to local hrefs
+      return this.href.match('\#.+'); 
+    }).on('click', function(ev) {
+        ev.preventDefault();
+        var href = $(this).attr('href');
+        document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
